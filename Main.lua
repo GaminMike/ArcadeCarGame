433f0a846706e377343e874ef87d47802573cc36
GaminMike <Mikaeltylerrife@gmail.com>
2024-03-23 01:27:30 -0400

    Fixed Memory Leak, Organized Code, Welcome Screen!

--- Main.lua
+++ Main.lua
@@ -2,36 +2,59 @@ p = love.physics
 g = love.graphics
 k = love.keyboard
 
-function SplashScreen()
-  print("welcome to the game")
-  background = g.newImage('sprites/SplashScreen.jpg')
-end
 
+function SplashScreen()
+  fadevalue = 0
+  love.window.setFullscreen(true)
+  local width, height = g.getDimensions()
+  local ScaleX = width/3840
+  local ScaleY = height/2160
+  g.draw(background, 0, 0, 0, ScaleX, ScaleY)
+  fadevalue = fadevalue + 1
+end
 
 function love.load()
-  SplashScreen()
-  love.window.setFullscreen(true)
+--Splash Screen background image loading
+
+  background = g.newImage('Images/TitleScreen.jpg')
+
+
+  -- Hello World Text
+  love.graphics.setFont (love.graphics.newFont (50))
+  font = love.graphics.getFont ()
+  text = love.graphics.newText(font)
+  text:add( {{0,0,0}, "Hello World!"}, 100, 100)
+
+--Fade Value For SplashScreen
+
+  fade = 0
+
+  width, height = g.getDimensions()
+
+  Center = {width/2, height/2}
 end
+
 
 function love.update(dt)
-  -- Keyboard Block
 
 
-     --[[
-  if k.isDown("up") or k.isDown("w") then
-    player.b:applyForce(0, -300) 
+  if k.isDown("escape") then
+    love.event.quit(1)
   end
-  if k.isDown("down") or k.isDown("s") then
-      player.b:applyForce(0, 300)
-  end
-  if k.isDown("left") or k.isDown("a") then
-    player.b:applyForce(-300, 0)
-  end
-  if k.isDown("right") or k.isDown("d") then
-    player.b:applyForce(300, 0)
-  end
-  ]]--
+
+
 end
+
+
+
 function love.draw()
-  g.draw(background, 0, 0)
+
+
+  love.graphics.setColor(1, 1, 1, fade)
+  SplashScreen()
+  love.graphics.draw (text, 10, 10)
+  if fade < 1 then
+    fade = fade + .01
+  end
+
 end
